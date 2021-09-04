a4c846b9ce4c73f999c54128f17cfef4d9941c1e
Fredy <freorozcoloa@gmail.com>
2021-09-04 16:21:02 -0500

    agregué algo aqui

--- control.php
+++ control.php
@@ -1,19 +1,16 @@
 <?php 
-    session_start();
-    require("ldap.php"); 
-    header("Content-Type: text/html; charset=utf-8");
-    $usr = $_POST["User"]; 
-    $usuario = mailboxpowerloginrd($usr,$_POST["Pass"]); 
-    if($usuario == '0' || $usuario == ''){
-        $_SERVER = array();
-        $_SESSION = array();
-        $_SESSION['error'] = 'Usuario o Contraseña incorrecta';
-        header('index.php');
-        return;
-    }
-    else{
-        $_SESSION["user"] = $usuario; 
-        $_SESSION["autentica"] = "SIP";
-        header('app.php');
-    }
+        require("ldap.php"); 
+        header("Content-Type: text/html; charset=utf-8"); 
+        $usr = $_POST["usuario"]; 
+        $usuario = mailboxpowerloginrd($usr,$_POST["clave"]); 
+        if($usuario == "0" || $usuario == ''){ 
+            $_SERVER = array(); 
+            $_SESSION = array(); 
+            echo"<script> alert('Usuario o clave incorrecta. Vuelva a digitarlos por favor.'); window.location.href='index.php'; </script>"; 
+        }else{ 
+            session_start(); 
+            $_SESSION["user"] = $usuario; 
+            $_SESSION["autentica"] = "SIP"; 
+            echo"<script>window.location.href='app.php'; </script>"; 
+        } 
 ?>
