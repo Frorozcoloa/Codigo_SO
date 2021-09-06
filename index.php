ff6d344f231d90ccb658caeb04838a886e636e50
Fredy <freorozcoloa@gmail.com>
2021-09-06 08:44:31 -0500

    Index hecho

--- index.php
+++ index.php
@@ -1,24 +1,28 @@
 <?php @session_start(); ?>
     <html>
     <head>
+    <link rel="stylesheet" type="text/css" href="main.css" media="screen" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <title>Acceso por LDAP</title>
     <body>
-    <form method="post" action="control.php"><br>
-        <table>
-            <tr>
-                <td>Usuario:</td>
-                <td><input type="text" maxlength="50" name="usuario" id="usuario" /></td>
-            </tr>
-            <tr>
-                <td>Clave:</td>
-                <td><input type="password" maxlength="50" name="clave" id="clave" />
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2"><input type="submit" value="Entrar"></td>
-            </tr>
-        </table><br>
-        </form>
+        <form method="post" action="control.php">
+            <div class="card text-center form-card">
+                <div class="card-body">
+                    <h4 class="card-title">Ingreso</h4>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="usuario">Usuario</label>
+                            <input  class="form-control" type="text" name="usuario" id="usuario" />
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="clave">Contraseña</label>
+                            <input  class="form-control" type="password" name="clave" id="clave" />
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-color btn-block">Ingresar</button>
+                </div>
+            </div>
+    </form>
     </body>
     </html>
 <?php exit(); ?>
